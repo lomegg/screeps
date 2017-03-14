625738bc0e4af9d551f158afddd6436da1bd2dd9
lomegg <unrelated@ya.ru>
2017-03-14 11:07:41 +0100

    cycle sources for harvesters

--- role.harvester.js
+++ role.harvester.js
@@ -1,14 +1,13 @@
 var roleChanger = require('script.roleChanger').selectRole;
-var sourceSelect = require('script.sourceSelector');
 var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
 
 
-        // set favorite source
-        if (!creep.memory.sourceId){
-            sourceSelect(creep);
+        // set source
+        if (!creep.memory.sourceId ){
+            creep.selectSource();
         }
 
         // deal with side job
@@ -30,16 +29,21 @@ var roleHarvester = {
 
             if(harvest_result == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.getObjectById(creep.memory.sourceId), {visualizePathStyle: {stroke: '#ffaa00'}});
-            } else if (harvest_result == ERR_NOT_ENOUGH_RESOURCES && creep.carry.energy > 0){
-
-                creep.say('drop rest');
-                creep.storeEnergy();
+            } else if (harvest_result == ERR_NOT_ENOUGH_RESOURCES){
+                if (creep.carry.energy > 0){
+                    creep.say('drop rest');
+                    creep.storeEnergy();
+                } else {
+                    creep.cycleSources();
+                }
             }
         }
         else if (creep.carry.energy > 0){
             // unload
-            creep.say('drop all');
-            creep.storeEnergy();
+            var storageResult = creep.storeEnergy();
+            if (typeof storageResult == 'undefined'){
+                roleChanger(creep);
+            }
         } else {
             creep.memory.unloading = false;
         }
