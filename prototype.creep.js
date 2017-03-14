787443e1c8f559989b0d6436440a49216647f186
lomegg <unrelated@ya.ru>
2017-03-14 01:41:43 +0100

    adding source selector to creep proto

--- prototype.creep.js
+++ prototype.creep.js
@@ -54,6 +54,66 @@ Creep.prototype.getStorageTarget = function(){
     return storageTarget;
 };
 
+/*
+* Select source based on range and capacity
+* */
+Creep.prototype.selectSource = function(){
+
+    delete this.memory.sourceId;
+
+    var spawn = Game.spawns['Spawn1'];
+    var creep = this;
+    var sources = spawn.room.find(FIND_SOURCES,{
+            filter: (structure) => {
+                return (structure.energy > structure.energyCapacity*0.05);
+            }
+    });
+
+    if (sources.length == 1){
+        this.setSource(sources[0].id);
+    } else if (sources.length > 1){
+
+
+        var sourceSpots = [4, 3],
+            sourcesMap = [];
+
+        // find out how much creeps using which spot
+        _.forEach(sources, function(source, index){
+            //console.log('spot', source, 'index', index);
+
+            console.log(creep, creep.name);
+
+            sourcesMap[index] = {
+                source: source,
+                spots: sourceSpots[index],
+                users: creep.room.find(FIND_MY_CREEPS, {filter: (creep) => { return creep.memory.sourceId == source.id }}).length,
+                distance: creep.pos.findPathTo(source).length
+            };
+
+            console.log('users', sourcesMap[index].users, 'distance', sourcesMap[index].distance, 'spots', sourcesMap[index].spots);
+
+            sourcesMap[index].rating = sourcesMap[index].users * -10 -sourcesMap[index].distance + sourcesMap[index].spots * 10;
+            console.log ('Rating for Source ' + sourcesMap[index].source.id + ' is ' + sourcesMap[index].rating);
+        });
+
+
+        var sorted = sourcesMap.sort(function(a,b) {return (a.rating < b.rating) ? 1 : ((b.rating < a.rating) ? -1 : 0);} );
+
+        console.log ('Target source:', sorted[0].source.id);
+
+        creep.say('new src');
+        creep.setSource(sorted[0].source.id);
+    }
+
+};
+
+/*
+ * Set source for creep
+ * */
+Creep.prototype.setSource = function(sourceId){
+   this.memory.sourceId = sourceId;
+};
+
 
 /**
  * Store the energy into the target storage
